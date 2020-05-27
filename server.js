dea733a506342fe1ab55695ed6ba03094ca39ce6
FaelDi <faeldii@icloud.com>
2020-05-27 15:41:35 -0300

    adicionando varias alteracoes ao projeto

---
+++ server.js
@@ -0,0 +1,23 @@
+// Levantando os parâmetros de ambiente
+require("dotenv").config();
+
+// Trazendo dependências
+const path = require("path");
+const express = require('express');
+
+// Trazendo routers
+const AuthRouter = require("./routes/AuthRouter");
+
+// Criando o app express
+const app = express();
+
+app.use(express.json());
+
+// Configurando pasta public para requisições estáticas
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api/', AuthRouter);
+
+// Levantando o servidor
+app.listen(process.env.HTTP_PORT);

ff875f376ad27dcbbc463e5687a8926f9c464e70
FaelDi <faeldii@icloud.com>
2020-05-27 16:35:31 -0300

    adicionando require

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,17 @@
+const { Usuario } = requier("../models");
+const bcrypt = require("bcrypt");
+
 module.exports = {
-    login:(req,res) => {
+    login: async (req, res) => {
+        const usuario = await Usuario.findOne({ where: { email: req.body.email } });
         
-    }
+        if (!usuario) {
+        return res.status(403).json({ error: "Login inválido" });
+        }
+        const senhaValida = bcrypt.compareSync(req.body.senha, usuario.senha);
+        if (!senhaValida) {
+        return res.status(403).json({ error: "Login inválido" });
+        }
+        return res.status(200).json({ msg: "sucesso" });
+        } 
 }
